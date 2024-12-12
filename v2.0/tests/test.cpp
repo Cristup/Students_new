14be119615f5447090fcced490c9aea0f4154ab3
Cristup <cristupkmz@gmail.com>
2024-12-13 01:12:58 +0200

    Student Class Testing

--- v2.0/tests/test.cpp
+++ v2.0/tests/test.cpp
@@ -1,18 +1,62 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include <vector>
-#include <numeric>
 
-double final_ave(std::vector<int> homeworks, int exam)
-{
-	return (std::accumulate(homeworks.begin(), homeworks.end(), 0.0) / homeworks.size()) * 0.4 + exam * 0.6;
+#include "../include/libs.h"
+#include "../include/objects.h"
+
+
+
+TEST(RecordTest, Initialization) {
+	Record rec;
+	EXPECT_EQ(rec.input, 0.0);
+	EXPECT_EQ(rec.sorting, 0.0);
+	EXPECT_EQ(rec.categorising, 0.0);
+	EXPECT_EQ(rec.output, 0.0);
+	EXPECT_EQ(rec.total, 0.0);
+	EXPECT_EQ(rec.count, 0);
 }
 
-TEST(FinalResultAverageTest, WithGoodInput) {
-	std::vector<int> vec1 = { 1,2,3 };
-	std::vector<int> vec2 = { 8,4,6 };
-	double exam1 = 8;
-	double exam2 = 9;
-	EXPECT_EQ(final_ave(vec1, exam1), 5.6);
-	EXPECT_EQ(final_ave(vec2, exam2), 7.8);
+TEST(StudentTest, ParametrizedConstructor) {
+	student object("Jonas", "Jonaitis", { 10, 9, 8 }, 7);
+
+	EXPECT_EQ(object.name(), "Jonas");
+	EXPECT_EQ(object.surname(), "Jonaitis");
+	EXPECT_EQ(object.exam(), 7);
+	EXPECT_EQ(object.homeworks(), std::vector<int>({ 10, 9, 8 }));
+}
+
+TEST(StudentTest, CopyAssignmentOperator) {
+	student source("Jonas","Jonaitis", { 10, 9, 8 }, 7);
+	student target;
+	target = source;
+
+	EXPECT_EQ(target.name(), "Jonas");
+	EXPECT_EQ(target.surname(), "Jonaitis");
+	EXPECT_EQ(target.homeworks(), std::vector<int>({ 10, 9, 8 }));
+	EXPECT_EQ(target.exam(), 7);
+	EXPECT_DOUBLE_EQ(target.final_average(), source.final_average());
+	EXPECT_DOUBLE_EQ(target.final_median(), source.final_median());
+}
+
+TEST(StudentTest, FinalResult) {
+	student source("Jonas", "Jonaitis", { 10, 9, 8 }, 7);
+
+	double expectedResult = 0.4 * 9 + 0.6 * 7;
+	EXPECT_EQ(source.final_average(), expectedResult);
+	EXPECT_EQ(source.final_median(), expectedResult);
+}
+
+TEST(IsDigitsTest, ValidStrings) {
+    EXPECT_TRUE(is_digits("123"));
+    EXPECT_TRUE(is_digits("000"));
+    EXPECT_TRUE(is_digits("456789"));
+    EXPECT_TRUE(is_digits("0"));
+}
+
+TEST(IsDigitsTest, InvalidStrings) {
+    EXPECT_FALSE(is_digits("abc"));  
+    EXPECT_FALSE(is_digits("123a"));  
+    EXPECT_FALSE(is_digits("12#45"));
+    EXPECT_FALSE(is_digits("12.45"));
+    EXPECT_FALSE(is_digits("12 45"));
 }
